cb1ebce65e7cc6db85982e0adf847480bd1f17bb
Dmitri Maksimov <dmitri.maksimov@dynatrace.com>
2025-12-01 10:49:51 +0200

    Day01 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,56 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var counter = 0
+        var currentSum = 50
+        for (instruction in input) {
+            if (instruction.startsWith("L")) {
+                currentSum -= instruction.substring(1).toInt()
+            }
+            if (instruction.startsWith("R")) {
+                currentSum += instruction.substring(1).toInt()
+            }
+            currentSum %= 100
+
+            if (currentSum == 0) {
+                counter++
+            }
+        }
+        return counter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        var counter = 0
+        var currentSum = 50
+        for (instruction in input) {
+            if (instruction.startsWith("L")) {
+                currentSum -= instruction.substring(1).toInt()
+            }
+            if (instruction.startsWith("R")) {
+                currentSum += instruction.substring(1).toInt()
+            }
+            val fullTurns = abs(currentSum / 100)
+            counter += fullTurns
+            currentSum %= 100
+            if (currentSum < 0) {
+                counter++
+                currentSum += 100
+            }
+        }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        return counter
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println("testInput = ${testInput}")
+    val part1 = part1(testInput)
+    println("part1 = ${part1}")
+    check(part1 == 3)
+    val part2 = part2(testInput)
+    println("part2 = ${part2}")
+    check(part2 == 6)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
